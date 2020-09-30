0a7d889e93cbc8d47d53679c5057aab5b6a47ff6
unknown <cindy50633@gmail.com>
2020-09-30 11:38:38 +0900

    add skeleton framework

---
+++ cindy50633/public/index.php
@@ -0,0 +1,19 @@
+<?php
+$test = 'cindy';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+<button>
+    <?=$test?>
+</button>
+
+</body>
+</html>
